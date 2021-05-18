0ca193e47737ca24a24674477b9270b2f9d1a522
GitHub Action <action@github.com>
2021-05-18 07:48:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/hcyandroid/bkdxmoban/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hcyandroid/bkdxmoban/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hcyandroid.bkdxmoban.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hcyandroid.bkdxmoban.MyBundle
 
 class MyApplicationService {
 
